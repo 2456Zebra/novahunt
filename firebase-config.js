836fd4ab45d4e9a9d0638884b03360e8943d94c7
2456Zebra <2456Zebra@gmail.com>
2025-10-31 09:40:48 -0700

    "firebase-config.js"

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+// src/firebase-config.js
+// REPLACE WITH YOUR REAL CONFIG LATER
+// Create free project: https://console.firebase.google.com/
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef123456"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
