a3da5d19d2a1d55b1638bd0c7ad4ce446ad8333c
2456Zebra <2456Zebra@gmail.com>
2025-11-09 16:06:24 -0800

    feat: global auth wrapper
    Added user subscription check and state management.

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,21 @@
+import { useEffect, useState } from 'react';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [isPro, setIsPro] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Check subscription on load
+    fetch('/api/user/subscription')
+      .then(res => res.json())
+      .then(data => {
+        setIsPro(data.isPro);
+        // If no user, redirect to sign in
+        if (!data.user) window.location = '/signin';
+      });
+  }, []);
+
+  return (
+    <Component {...pageProps} isPro={isPro} user={user} />
+  );
 }
