684b63fbb1663ec02f97e8dc081f1b3de4a68bd5
2456Zebra <2456Zebra@gmail.com>
2025-11-13 14:09:36 -0800

    Revise Firebase config with placeholders
    Updated Firebase configuration with placeholder values.

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Update and paste your real Firebase config values here.
+// Make sure this file exports `auth` so components can import `auth`.
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCa66RRIPgCu5uemwH8vIgQBdlVe5ejStM",
-  authDomain: "novahunt-da390.firebaseapp.com",
-  projectId: "novahunt-da390",
-  storageBucket: "novahunt-da390.firebasestorage.app",
-  messagingSenderId: "564769701763",
-  appId: "1:564769701763:web:1b76daf5ac786f1d6100b6"
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_PROJECT.firebaseapp.com',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_PROJECT.appspot.com',
+  messagingSenderId: '123456789',
+  appId: '1:123456789:web:abcdef123456',
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export default app;
