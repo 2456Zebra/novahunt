2bc21607ef753152f488c532ea29fee7a94b9d12
2456Zebra <2456Zebra@gmail.com>
2025-11-13 19:03:10 -0800

    Simplify comment on dynamic header loading
    Removed comment about SSR/hydration mismatches for clarity.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import '../styles/globals.css';
 
-// Load header client-side to avoid SSR/hydration mismatches that caused React invariant errors
+// Load header client-side to avoid SSR/hydration mismatches
 const Header = dynamic(() => import('../components/Header'), { ssr: false });
 
 export default function MyApp({ Component, pageProps }) {
