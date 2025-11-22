7b902b9791284ec3e2ea55fe217ffb2453853cb3
2456Zebra <2456Zebra@gmail.com>
2025-11-22 13:45:24 -0800

    Remove unnecessary comments from SearchPage
    Removed comments that describe the purpose of domain string and CompanyProfile.

--- pages/search.js
+++ pages/search.js
@@ -4,7 +4,6 @@ import SearchResults from '../components/SearchResults';
 import CompanyProfile from '../components/CompanyProfile';
 
 export default function SearchPage() {
-  // domain string and result shape are provided by SearchClient via onResults
   const [domain, setDomain] = useState('');
   const [result, setResult] = useState({ items: [], total: 0, public: true });
 
@@ -27,7 +26,6 @@ export default function SearchPage() {
         </div>
 
         <div style={{ flex: '1 1 50%', minWidth: 300 }}>
-          {/* CompanyProfile can remain decorative and show domain/result context */}
           <CompanyProfile domain={domain} result={result} />
         </div>
       </div>
