a2fac58b236787806caee9cb26c0a5adfee154da
2456Zebra <2456Zebra@gmail.com>
2025-11-25 10:28:02 -0800

    Create CorporateProfile as a compatibility wrapper
    Re-export CompanyProfile to maintain compatibility.

---
+++ components/CorporateProfile.jsx
@@ -0,0 +1,5 @@
+import CompanyProfile from './CompanyProfile';
+
+// Compatibility wrapper: re-export CompanyProfile so existing imports of
+// "CorporateProfile" keep working until we refactor all imports.
+export default CompanyProfile;
