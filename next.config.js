b759a8bef61b77bbb39191345a737febfb64460d
2456Zebra <2456Zebra@gmail.com>
2025-11-26 13:53:44 -0800

    Update comment for ESLint build skipping

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // next.config.js
-// Temporarily skip ESLint during build to unblock deployment while we fix lint issues.
+// Temporarily skip ESLint during build to unblock deployment while we clean up lint issues.
 module.exports = {
   eslint: {
     ignoreDuringBuilds: true
