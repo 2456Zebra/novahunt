a9c22becd5083ebf85cb46fdd9a8482e37b7370f
2456Zebra <2456Zebra@gmail.com>
2025-11-26 11:39:05 -0800

    Fix formatting in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 // next.config.js
 // Temporarily skip ESLint during build to unblock deployment while we fix lint issues.
-// Remove or set ignoreDuringBuilds: false once lint is cleaned up.
 module.exports = {
   eslint: {
-    ignoreDuringBuilds: true,
-  },
+    ignoreDuringBuilds: true
+  }
 };
